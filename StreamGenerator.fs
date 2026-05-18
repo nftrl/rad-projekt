a5f29926d10601b4db26371885263b50b7e5c54a
PerGarner <perhindborg@gmail.com>
2026-05-18 19:52:25 +0200

    Added StreamGenerator code from project description

--- StreamGenerator.fs
+++ StreamGenerator.fs
@@ -1,1 +1,31 @@
 module StreamGenerator
+// Code from project description
+
+let createStream (n : int) (l : int) : seq<uint64 * int> =
+    seq {
+        // We generate a random uint64 number.
+        let rnd = System.Random()
+        let mutable a = 0UL
+        let b : byte [] = Array.zeroCreate 8
+        rnd.NextBytes(b)
+        let mutable x : uint64 = 0UL
+        for i = 0 to 7 do
+            a <- ( a <<< 8) + uint64(b.[i])
+
+        // We demand that our random number has 30 zeros on the least
+        // significant bits and then a one .
+        a <- (a ||| ((1UL <<< 31) - 1UL) ) ^^^ ((1UL <<< 30) - 1UL)
+
+        let mutable x = 0UL
+        for i = 1 to (n/3) do
+            x <- x + a
+            yield (x &&& (((1UL <<< l ) - 1UL) <<< 30), 1)
+
+        for i = 1 to ((n + 1)/3) do
+            x <- x + a
+            yield ( x &&& (((1UL <<< l ) - 1UL) <<< 30), -1)
+
+        for i = 1 to (n + 2)/3 do
+            x <- x + a
+            yield (x &&& (((1UL <<< l ) - 1UL) <<< 30), 1)
+    }
