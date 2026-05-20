2c5ef89f312841380a1ad94e0aec9c56dee5b564
nftrl <marcuslarsen97@gmail.com>
2026-05-20 11:04:49 +0200

    mindre ting

--- Program.fs
+++ Program.fs
@@ -11,14 +11,14 @@ let b = 476198I
 let h_multiplyShift = multiplyShift a 5
 let h_multiplyModPrime = multiplyModPrime (bigint a) b 32
 
+printfn "stream:"
 for (x, dx) in stream do
-    printfn "stream:"
     printfn "%i, %i" x dx
 
+printfn "multiply shift"
 for (x, _) in stream do
-    printfn "multiply shift"
     printfn "%i, %A" x (h_multiplyShift x)
 
+printfn "multiply mod prime"
 for (x, _) in stream do
-    printfn "multiply mod prime"
     printfn "%i, %A" x (h_multiplyModPrime x)
