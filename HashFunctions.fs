ec880f6b2c0e1ff5a9a81231091e7dd2caa339c2
nftrl <marcuslarsen97@gmail.com>
2026-05-20 11:02:32 +0200

    change to uint64

--- HashFunctions.fs
+++ HashFunctions.fs
@@ -1,19 +1,20 @@
 module HashFunctions
 
-let multiplyShift (a : int64) (l : int) (x : int64) : int64 =
+let multiplyShift (a : uint64) (l : int) (x : uint64) : uint64 =
     // assumes:
     //   a uneven
     //   0 < l < 64
     (a * x) >>> (64 - l)
 
-let multiplyModPrime (a : bigint) (b : bigint) (l : int) (x : bigint) : bigint =
+let multiplyModPrime (a : bigint) (b : bigint) (l : int) (x : uint64) : bigint =
     // assumes:
     //   a < p
     //   b < p
     //   0 < l < 64
     let q : int = 89
     let p : bigint = (2I ** q) - (1I)
-    x |> fun y -> a * y + b                   // ax + b
+    x |> bigint
+      |> fun y -> a * y + b                   // ax + b
       |> fun y -> (y &&& p) + (y >>> q)
       |> fun y -> if y >= p then y - p else y // (ax + b) mod p
       |> fun y -> y &&& (2I ** l - 1I)        // (ax + b) mod p mod m
