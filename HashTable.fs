10335e8c8f01ea28e414bfd1315965b23a8f99ae
nftrl <marcuslarsen97@gmail.com>
2026-05-21 08:45:46 +0200

    try impl 2a
    fails with "error FS0193: Specified method is not supported."

--- HashTable.fs
+++ HashTable.fs
@@ -1,1 +1,19 @@
 module HashTable
+
+type HashFunction = uint64 -> uint64
+
+let rec findInList (key : uint64) (xs : (uint64 * 'a) list) : 'a option =
+    match xs with
+    | (y,v) :: ys -> if y = key then Some v else findInList key xs
+    | [] -> None
+
+type HashTable(h : HashFunction, l : int) =
+    let array : ((uint64 * uint64) list) [] = Array.create (1 <<< l) []
+    let h = h
+
+    member this.Item
+        with get (x : uint64) : uint64 =
+            let key_x = h x
+            match findInList key_x array.[(int key_x)] with
+            | Some v -> v
+            | None -> 0UL
