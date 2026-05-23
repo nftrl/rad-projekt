6ec58ad2348398817c67684667ac9b088e50dadf
PerGarner <perhindborg@gmail.com>
2026-05-23 09:12:20 +0200

    TestRuntimeofhashfunctions integrated into project. <<menu>> added to dotnet run

--- Program.fs
+++ Program.fs
@@ -1,24 +1,43 @@
 ﻿open HashFunctions
 open StreamGenerator
 open RandomBytes
+open TestRuntimeOfHashFunctions
 
+let simpleExample() =
+    let stream = createStream 10 10
 
-let stream = createStream 10 10
+    let rnd = RandomSource("RandomNumbers.data")
 
-let rnd = RandomSource("RandomNumbers.data")
+    let h_multiplyShift = randomMultiplyShift rnd 5
 
-let h_multiplyShift = randomMultiplyShift rnd 5
+    let h_multiplyModPrime = randomMultiplyModPrime rnd 32
 
-let h_multiplyModPrime = randomMultiplyModPrime rnd 32
+    printfn "stream:"
+    for (x, dx) in stream do
+        printfn "%i, %i" x dx
 
-printfn "stream:"
-for (x, dx) in stream do
-    printfn "%i, %i" x dx
+    printfn "multiply shift"
+    for (x, _) in stream do
+        printfn "%i, %A" x (h_multiplyShift x)
 
-printfn "multiply shift"
-for (x, _) in stream do
-    printfn "%i, %A" x (h_multiplyShift x)
+    printfn "multiply mod prime"
+    for (x, _) in stream do
+        printfn "%i, %A" x (h_multiplyModPrime x)
 
-printfn "multiply mod prime"
-for (x, _) in stream do
-    printfn "%i, %A" x (h_multiplyModPrime x)
+let args = System.Environment.GetCommandLineArgs()
+let options = [| 
+    "Simple Example", simpleExample;
+    "Test Runtime of Hash Functions", TestRuntime
+|]
+if Array.length args < 2 then
+    printfn "No command line argument given."
+    printfn "Run with 'dotnet run [option]. The options are"
+    for i in 0 .. Array.length options - 1 do
+        printfn "%i: %A" i (fst options[i])
+else
+    let success, option = System.Int32.TryParse args[1]
+    if success && option < Array.length options then
+        snd options[option] ()
+    else
+        printfn "Wrong option."
+
--- TestRuntimeOfHashFunctions.fs
+++ TestRuntimeOfHashFunctions.fs
@@ -1,28 +1,40 @@
+module TestRuntimeOfHashFunctions
+
 open HashFunctions
 open StreamGenerator
+open System.Diagnostics
 
-let n = 1000
-let stream = createStream n 1
+let TestRuntime() =
+    let n = 10000
+    let stream = createStream n 1
 
-let printFunc stream (h : uint64 -> bigint) =
-    stream
-        |> Seq.map (fun (x,_) -> x)
-        |> Seq.map h
-        |> Seq.sum
-        |> printfn "%A"
+    let printFunc stream (h : uint64 -> bigint) =
+        stream
+            |> Seq.map (fun (x,_) -> x)
+            |> Seq.map h
+            |> Seq.sum
+            |> printfn "%A"
 
-printfn "Calculating sum(h(x_i)) for different h"
+    printfn "Calculating sum(h(x_i)) for different h"
 
-// 0 < l < 64
-let l = 32
+    // 0 < l < 64
+    let l = 32
 
-printfn "multiply shift"
-// a odd r.v.
-let a = 12381UL
-printFunc stream (fun x -> x |> multiplyShift a l |> bigint)
+    printfn "multiply shift"
+    // a odd r.v.
+    let a = 12381UL
+    let timer = new Stopwatch()
+    timer.Start()
+    printFunc stream (fun x -> x |> multiplyShift a l |> bigint)
+    timer.Stop()
+    printfn "Time elapsed %A ms" timer.ElapsedMilliseconds
 
-printfn "multiply mod prime"
-// b,c r.v.s less than 2^89 - 1
-let b = 9163203I
-let c = 476198I
-printFunc stream (multiplyModPrime b c l)
+    printfn "multiply mod prime"
+    // b,c r.v.s less than 2^89 - 1
+    let b = 9163203I
+    let c = 476198I
+    timer.Reset()
+    timer.Start()
+    printFunc stream (multiplyModPrime b c l)
+    timer.Stop()
+    printfn "Time elapsed %A ms" timer.ElapsedMilliseconds
