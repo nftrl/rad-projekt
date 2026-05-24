67d7230f56884edbf0405cdf4b58f89193171b62
PerGarner <perhindborg@gmail.com>
2026-05-24 19:55:10 +0200

    update readme, min_l added in calculatesquaresums

--- CalculateSquareSums.fs
+++ CalculateSquareSums.fs
@@ -20,12 +20,13 @@ let getTimeAndSqSum (h: HashFunction) (n: int) (l: int) : (int64 * uint64) =
 
 
 let CalculateSquaresums() =
-    let n = 1<<<25
-    let max_l = 25
+    let n = 1<<<20
+    let min_l = 1
+    let max_l = 20  
     let rnd = RandomSource("RandomNumbers.data")
 
     printfn "n=%d" n
-    for l in 1 .. max_l do
+    for l in min_l .. max_l do
         let h_ms = randomMultiplyShift rnd l
         let (ms_time, ms_sqSum) = getTimeAndSqSum h_ms n l
         let hBig = randomMultiplyModPrime rnd l
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ rad 2026 implementeringsprojekt
 
 ## TODO
 - Opgave 1 + 2 er ok (tror jeg)
-
+- Jeg har også lavet opg. 3 - er dog i tvivl om det med l-værdi hvor "algoritmen ikke terminerede". Det ser ud til at køre, men bliver selvfølgelig langsommere med høj n, så har kørt med n=2^20 /Per.
